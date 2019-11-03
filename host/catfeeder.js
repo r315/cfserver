afc29e322339b59e16f6ab155daf6e544fae221b
r315 <a38652@alunos.isel.pt>
2019-11-03 21:31:02 +0000

    ADD: posr request to server

--- host/catfeeder.js
+++ host/catfeeder.js
@@ -11,6 +11,7 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 const path = require('path');
+const fs = require('fs')
 
 /**
  * Console commands
@@ -49,22 +50,16 @@ app.get('/',(req, res) => {
     res.sendFile(path.join(__dirname+'/../public/index.html'));
 })
 
-app.get('/schedules',(req, res) => {
-    let sch = [
-        {
-        "qnt" : 0,
-        "repeat": 24,
-        "active" : true,
-        "dateTime": "20:00"        
-    },
-    {
-        "qnt" : 0,
-        "repeat": 24,
-        "active" : true,
-        "dateTime": "08:00"        
-    },
-]
-    res.send(JSON.stringify(sch));
+app.get('/schedule',(req, res) => {
+    res.sendFile(path.join(__dirname+'/../public/schedule.json'));
+})
+
+app.post('/schedule',(req, res) => {
+    let filePath = path.join(__dirname+'/../public/schedule.json')
+
+    fs.writeFile(filePath, JSON.stringify(req.body), () => {
+            res.end();
+    });
 })
 
 app.post('/',(req, res) => {
