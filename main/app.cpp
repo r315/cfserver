339a725c874e7397b64affe1bb646211fa9ffbb2
r315 <a38652@alunos.isel.pt>
2019-11-03 21:35:21 +0000

    ADD: schedules list

--- main/app.cpp
+++ main/app.cpp
@@ -18,6 +18,7 @@
 #include "task.h"
 #include "queue.h"
 
+#include "list.h"
 #include "cfserver.h"
 #include "stepper.h"
 #include "repo.h"
@@ -29,14 +30,12 @@ static const char *TAG="App";
 static Cfserver server;
 
 typedef struct _schedule_t{
-    uint16_t qnt;
-    uint64_t time;
-    uint8_t repeat;
+    uint16_t qnt;              //5 chars
+    uint8_t repeat;            //3 chars
+    uint64_t time;             //20 chars
 }schedule_t;
 
-#define MAX_SCHEDULES 5
-
-QueueHandle_t schedulers;
+#define SCHEDULE_T_CHARS    28
 
 
 void onWifiConnected(void){
@@ -47,8 +46,7 @@ void onWifiDisconnect(void){
     
 }
 
-extern "C" void server_init(void)
-{    
+extern "C" void server_init(void){    
     char *ptr;
     uint8_t tmp[64];
     Json json;    
@@ -72,55 +70,74 @@ extern "C" void server_init(void)
     server.onDisconnect = onWifiDisconnect;
 }
 
+#if 0
+static void schToString(char *dst, schedule_t *sch){
+    memcpy(dst, "{}", 2);     
+}
 
-static void loadSchedules(void){
-char *ptr;
-uint8_t str[20];
+static void saveSchedulers(node_t *head){
+    uint32_t count = countNodes(head);
+    char *jstr = (char*)malloc((SCHEDULE_T_CHARS + 2) * count);
+    *(jstr++) = '[';
+    for(uint32_t i = 0; i < count; i++){
+        schedule_t *sch = (schedule_t*)(head->next->value);
+        schToString(jstr, sch);
+        jstr +=SCHEDULE_T_CHARS + 2; // plus brackets
+    }
+}
+#endif
+
+static void loadSchedules(node_t *head){
 Json json;
+char *jstr;
+uint8_t tmp[10];
 
-    if(schedulers == NULL){
-        schedulers = xQueueCreate(MAX_SCHEDULES, sizeof(schedule_t));
-    }
-
-    if(schedulers == NULL){
-        ESP_LOGE(TAG, "Unable to create schedulers queue");
-        return;
-    }
-
-    if(REPO_GetSchedules(&ptr) > 0){
-        ESP_ERROR_CHECK(json.init(ptr));
+    if(REPO_GetSchedules(&jstr) > 0){
+        ESP_ERROR_CHECK(json.init(jstr));
         
         while(json.nextToken(JSMN_OBJECT)){
-            if(json.string("qnt", str) > 0){
-                printf("qnt: %s\n", str);
-            }
+            schedule_t *sch = (schedule_t*)malloc(SCHEDULE_T_CHARS);
+		    if(sch == NULL){
+			    ESP_LOGE(TAG,"Unable to allocate schedule");			    
+		    }else{
+                if(json.string("qnt", tmp) > 0){
+                    sch->qnt = atoi((const char *)tmp);                    
+                }
 
-            if(json.string("repeat", str) > 0){
-                printf("repeat: %s\n", str);
-            }
-            
-            if(json.string("active", str) > 0){
-                printf("active: %s\n", str);
-            }
+                if(json.string("repeat", tmp) > 0){
+                    sch->repeat = atoi((const char *)tmp);                    
+                }
 
-            if(json.string("time_t", str) > 0){
-                printf("time_t: %s\n", str);
+                if(json.string("time_t", tmp) > 0){
+                    sch->time = atol((const char *)tmp);                    
+                }                
+                node_t *node = createNode(sch);
+                if(node == NULL){
+			        ESP_LOGE(TAG,"Unable to allocate node");
+                }else{
+                    insertTail(head, node);
+                }
             }
-
         }
-        free(ptr);
+        free(jstr);
     }
 }
 
 extern "C" void app(void){
-char *ptr;
+time_t now;
+schedule_t *sch;
+node_t list;
 
     ESP_LOGI(TAG, "Running Application");
-
-    schedulers = NULL;
-    loadSchedules();
+    
+    loadSchedules(&list);
 
     while(1){
+        now = SNTP_GetTime();
+        sch = (schedule_t*)(list.next->value);
+        if(sch->time == now){            
+                printf("feeding\n");
+        }        
         vTaskDelay(1000);
     }
 }
