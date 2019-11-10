a28932be7c520a58b3eb31de4bf7ebbe71c8bc9c
r315 <a38652@alunos.isel.pt>
2019-11-10 23:37:40 +0000

    add updated package files

--- host/catfeeder.js
+++ host/catfeeder.js
@@ -13,6 +13,8 @@ const app = express()
 const path = require('path');
 const fs = require('fs')
 
+const schedulesFile = path.join(__dirname+'/../public/schedule.json')
+
 /**
  * Console commands
  */
@@ -42,32 +44,66 @@ function commandInputHandler(buffer){
 /**
  * Application entry point
  */
+let schedules = []
 
-//app.get('/',(req, res) => {res.send('hello from server')})
-app.use(bodyParser());
+function loadSchedules(){
+    fs.readFile(schedulesFile, (err, data)=> {
+        if(!err && data.length > 0)
+            schedules = JSON.parse(data)
+        else
+            schedules = []
+    })
+}
+
+function saveSchedules(cb){
+    fs.writeFile(schedulesFile, JSON.stringify(schedules), (err)=>{
+        if(!err)
+            cb("ok")
+        else
+            cb("fail")
+    })
+}
+
+app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({extended: true}));
+
 app.get('/',(req, res) => {
     //res.setHeader("content-encoding","gzip")
     res.sendFile(path.join(__dirname+'/../public/index.html'));
-})
-
-app.get('/schedule',(req, res) => {
-    res.sendFile(path.join(__dirname+'/../public/schedule.json'));
-})
-
-app.post('/schedule',(req, res) => {
-    let filePath = path.join(__dirname+'/../public/schedule.json')
-
-    fs.writeFile(filePath, JSON.stringify(req.body), () => {
-            res.end();
-    });
 })
 
 app.post('/',(req, res) => {
     log(`Dispense: ${req.body.qnt}`, true);
     //res.contentType = "text/plain"
     res.header("Content-Type", "text/plain");
-    res.end("ok")})
+    res.end("ok")
+})
+
+
+app.get('/schedule',(req, res) => {
+    //res.sendFile(path.join(__dirname+'/../public/schedule.json'));
+    res.send(JSON.stringify(schedules))
+})
+
+app.post('/schedule',(req, res) => {
+    sch = req.body
+    log(sch.dateTime, true)
+    schedules.push(sch)
+    saveSchedules((msg)=> { res.send(msg)})
+})
+
+app.delete('/schedule', (req, res) => {
+    let index = req.body.index - 1 // remove index offset
+    log(`delete request for index ${index}`, true)
+    schedules.splice(index,1)
+    res.end()
+})
+
 process.openStdin().addListener('data', commandInputHandler)
+
 commandOut(prompt)
+
 app.listen(port, () => log(`Example app listening on port ${port}!`, true));
 //startServer(process.argv[2])
+
+loadSchedules()
