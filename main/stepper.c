0e3cf8d231aac4c3de2d1acb2b41c210796af609
r315 <a38652@alunos.isel.pt>
2019-11-12 22:22:28 +0000

    Added new  functions to stepper API

--- main/stepper.c
+++ main/stepper.c
@@ -10,24 +10,14 @@
 #include "driver/gpio.h"
 #include "esp_console.h"
 
-
-#define STEP_CLK_PIN    14
-#define STEP_DIR_PIN    12
-#define STEP_EN_PIN     13
-
-#define STEP_CW         0
-#define STEP_CCW        1
-
-#define STEP_DISABLED   0
-#define STEP_ENABLED    1
-
-#define STEP_DEFAULT_PULSE  7000
-#define STEP_MIN_PULSE      1000
-
 static xQueueHandle stepper_evt_queue = NULL;
 static const char *TAG = "STEP";
 
-static uint32_t pulse_width;
+StpMove default_stp = {
+    0,
+    STEP_DEFAULT_PULSE,
+    STEP_CW
+};
 
 static void pulseStep(uint32_t pulse, uint32_t pulses, uint8_t dir){
     gpio_set_level(STEP_DIR_PIN, dir);
@@ -41,16 +31,15 @@ static void pulseStep(uint32_t pulse, uint32_t pulses, uint8_t dir){
     gpio_set_level(STEP_EN_PIN, STEP_DISABLED);
 }
 
-
 static void stepperHandler(void *queue)
 {
-    uint32_t steps;
+    StpMove stp;
     xQueueHandle *stepper_evt_queue = (xQueueHandle*)queue;
 
     for (;;) {
-        if (xQueueReceive(stepper_evt_queue, &steps, portMAX_DELAY)) {
-            ESP_LOGI(TAG, "Moving [%d] steps", steps);            
-            pulseStep(pulse_width, steps, STEP_CW);            
+        if (xQueueReceive(stepper_evt_queue, &stp, portMAX_DELAY)) {
+            ESP_LOGI(TAG, "Moving [%d] steps", stp.pulses);            
+            pulseStep(stp.time, stp.pulses, STEP_CW);            
         }
     }
 }
@@ -71,61 +60,24 @@ void STEP_Init(void){
 
     gpio_set_level(STEP_EN_PIN, STEP_DISABLED);
 
-    pulse_width = STEP_DEFAULT_PULSE;
-    stepper_evt_queue = xQueueCreate(QUEUE_SIZE, sizeof(uint32_t));
+    stepper_evt_queue = xQueueCreate(QUEUE_SIZE, sizeof(StpMove));
     xTaskCreate(stepperHandler, "stepper_task", 2048, stepper_evt_queue, 10, NULL);
 }
 
 void STEP_MoveSteps(uint32_t steps){
-    xQueueSend(stepper_evt_queue, &steps, 0);
+    default_stp.pulses = steps;
+    xQueueSend(stepper_evt_queue, &default_stp, 0);
 }
 
-static int set_pulse(int argc, char **argv){
-uint32_t pulse;
-
-    if(argc < 2){
-        ESP_LOGI(TAG,"Current pulse: %u",pulse_width);
-        return 0;
-    }
-
-
-    pulse = atoi(argv[1]);
-
-    if(pulse < STEP_MIN_PULSE)
-    {
-        ESP_LOGI(TAG,"Invalid pulse\n");    
-        return 0;
-    }
-
-    ESP_LOGI(TAG,"Setting pulse to: %uus\n", pulse);    
-    pulse_width = pulse;
-    return 0;
+void STEP_SetPulseWidth(uint32_t time){
+    default_stp.time = time;
 }
 
-static int move_steps(int argc, char **argv){
-uint32_t steps = atoi(argv[1]);
-    ESP_LOGI(TAG,"moving %u steps\n", steps);    
-    STEP_MoveSteps(steps);
-    return 0;
+void STEP_Move(uint32_t steps, uint32_t time, uint8_t dir){
+    StpMove stp;
+    stp.pulses = steps;
+    stp.time = time;
+    stp.dir = dir;
+    xQueueSend(stepper_evt_queue, &stp, 0);
 }
 
-/**
- * Stepper console command
- * */
-void register_stepper(void){
-    const esp_console_cmd_t step_pulse = {
-        .command = "step_pulse",
-        .help = "Set step pulse width in us (>1000)",
-        .hint = NULL,
-        .func = &set_pulse,
-    };
-    ESP_ERROR_CHECK( esp_console_cmd_register(&step_pulse) );
-
-    const esp_console_cmd_t step_move = {
-        .command = "step_move",
-        .help = "Move in steps",
-        .hint = NULL,
-        .func = &move_steps,
-    };
-    ESP_ERROR_CHECK( esp_console_cmd_register(&step_move) );
-}
--- main/stepper.h
+++ main/stepper.h
@@ -14,10 +14,30 @@ extern "C" {
 
 
 #define QUEUE_SIZE 5
+#define STEP_CLK_PIN    14
+#define STEP_DIR_PIN    12
+#define STEP_EN_PIN     13
+
+#define STEP_CW         0
+#define STEP_CCW        1
+
+#define STEP_DISABLED   0
+#define STEP_ENABLED    1
+
+#define STEP_DEFAULT_PULSE  7000
+#define STEP_MIN_PULSE      1000
+
+
+typedef struct _StpMove{
+    uint32_t pulses;
+    uint32_t time;
+    uint8_t dir;
+}StpMove;
 
 void STEP_Init(void);
 void STEP_MoveSteps(uint32_t steps);
-void register_stepper(void);
+void STEP_SetPulseWidth(uint32_t time);
+void STEP_Move(uint32_t steps, uint32_t time, uint8_t dir);
 
 #ifdef __cplusplus
 }
